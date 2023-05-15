2284da49e25134c7336dbb5b08e4216e8de3da60
Alexander Lurita <alex11lurita@gmail.com>
2023-05-15 08:49:44 -0500

    Inicio proyecto reporte

---
+++ models/Conexion.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conexion{
+
+  private function Conectar(){
+    try{
+      $pdo = new PDO("mysql:host=localhost;port=3306;dbname=superhero;charset=UTF8","root","");
+      return $pdo;
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function getConexion(){
+    try{
+      $pdo = $this->Conectar();
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+}
+
+?>
